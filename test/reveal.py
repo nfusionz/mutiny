bc286f806c9455f607d20f2e9b87aac838bd7b19
Owen Xie <owenxie24@gmail.com>
2020-07-11 21:07:52 -0400

    Push reveal test with circular dep issue

--- test/reveal.py
+++ test/reveal.py
@@ -1,9 +1,37 @@
 import unittest
 
+from mutiny.game_data import GameData
+from mutiny.player import Player
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+from mutiny.actions import NoOp
+from mutiny.states.player_turn import *
+from mutiny.states.reveal import *
+
+
+class RevealTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.players = ["A", "B", "C", "D", "E", "F"]
+
+    def test_noop_reveal_first_card(self) -> None:
+        data = GameData([Player(name, i) for i, name in enumerate(self.players)])
+        data.player_turn = 0
+        data.reset()
+
+        state = Reveal(data=data, reveal_id=0, action=NoOp(data))
+        new_state = state.reveal(0, data.players[0].hand[0].role)
+
+        self.assertIsInstance(new_state, PlayerTurn,
+                              "Reveal with NoOp should have returned a PlayerTurn")
+        self.assertTrue(data.players[0].hand[0].revealed,
+                        "Player 0 should have his first influence revealed")
+        self.assertFalse(data.players[1].hand[1].revealed,
+                         "Player 0 should not have his second influence revealed")
+        for i in range(1, len(self.players)):
+            self.assertFalse(data.players[i].hand[0].revealed,
+                             "Player {} should not have influence 0 modified".format(i))
+            self.assertFalse(data.players[i].hand[0].revealed,
+                             "Player {} should not have influence 1 modified".format(i))
 
 
 if __name__ == '__main__':
