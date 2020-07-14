75c08aef2d1b2d6e977576b915947a19012647cb
Owen Xie <owenxie24@gmail.com>
2020-07-14 15:49:56 -0400

    Remove redundant computation

--- mutiny/game_object.py
+++ mutiny/game_object.py
@@ -26,7 +26,8 @@ class GameObject:
         return False
 
     def game_is_over(self):
-        return all([self.player_is_done(p) for p in range(len(self.players))])
+        return self.game_data.winner_id is not None
+        # return all([self.player_is_done(p) for p in range(len(self.players))])
 
     def to_dict(self,player_id=None):
         return self._state_interface.to_dict(player_id=player_id)
